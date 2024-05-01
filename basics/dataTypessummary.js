face3e7dee7862b4d9ce1617bb6bbcacca4eca0a
sandy <sandipwagle1300@gmail.com>
2024-05-01 11:43:52 +0545

    Update

---
+++ basics/dataTypessummary.js
@@ -0,0 +1,9 @@
+// primitive
+
+// 7 types : Number, string, boolean, undefined, null, symbol, BigInt
+
+
+// Reefrence (Non primitive)
+
+// Array, Objects, Functions
+console.log('datatype summary');
