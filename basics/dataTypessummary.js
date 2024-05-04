83c3f9bf9c4cda3bab756f3abb88dc582ed3ff6a
sandy <sandipwagle1300@gmail.com>
2024-05-04 17:14:30 +0545

    DataTypes summary

--- basics/dataTypessummary.js
+++ basics/dataTypessummary.js
@@ -3,7 +3,43 @@
 // 7 types : Number, string, boolean, undefined, null, symbol, BigInt
 
 
+const score = 100
+const scoreValue = 100.3   
+
+ 
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+
+
+const bigNumber = 7364897653689568976349853948538959834567n 
+//adingg n converts the data type into big int
+
+
+
 // Reefrence (Non primitive)
 
 // Array, Objects, Functions
-console.log('datatype summary');
+
+
+const animalName = ["tiger", "wolf", "gorilla"];
+let personalInformation = {
+    name: "John",
+    age: 18,
+}
+
+// treating function as variable
+
+const myFunction = function(){
+    console.log('Hello World');
+}
+
+
+
+
