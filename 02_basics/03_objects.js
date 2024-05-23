aad2ce642d0d3a8a8c9bd234f6a5f3bcbce29719
sandy <sandipwagle1300@gmail.com>
2024-05-23 19:48:46 +0545

    objects practice

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,36 @@
+// **** objects literals ****//
+
+
+const mySym = Symbol("Key1")
+
+
+const JsUser = {
+    name: "sandip",
+    "full Name": "sandip wagle",
+    [mySym]: "mykey1",
+    age: 20,
+    location: "kapan/kathmandu",
+    email: "sandy@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["wednesday", "thursday"]
+
+}
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full Name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "sandip.com.huf.ru"
+// Object.freeze(JsUser)
+JsUser.email = "sandip.ruhuh.ads.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello World");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello World, ${this.name}`);
+
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
