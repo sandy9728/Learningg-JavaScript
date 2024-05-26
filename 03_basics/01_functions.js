a286cc138f44983d97a6e130ad3f2bc07c40b701
sandy <sandipwagle1300@gmail.com>
2024-05-26 13:50:23 +0545

    practiced more functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -40,4 +40,36 @@
     }
 
     // console.log(loginUserMessage("Sandip Wagle"));
-    console.log(loginUserMessage("sandy"));
+    // console.log(loginUserMessage("sandy"));
+
+
+    function calculatecartprice(val1, val2, ...num1){
+        return num1
+    }
+// console.log(calculatecartprice(200, 300, 400));
+
+const user ={
+    username: "sandip",
+    price: 200
+}
+
+function handelobject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+    // handelobject(user)
+    handelobject({
+        username: "sandy",
+        price: 100
+    })
+
+
+const myNewArray = [200, 300, 400, 500]
+
+
+function returnsecondvalue(getArray){
+    return getArray[3]
+
+}
+// console.log(returnsecondvalue(myNewArray));
+console.log(returnsecondvalue([200, 300, 400, 500]));
