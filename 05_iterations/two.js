281ae84c5e552d763bc91af169c619ab106aaca9
sandy <sandipwagle1300@gmail.com>
2024-05-31 21:12:10 +0545

    Practiced about while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,36 @@
+//  **** while loop **** //
+
+let index = 0   
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`);
+    index = index + 2
+
+}
+
+
+let myArr = ["Ironman", "Superman", "Batman", "Spiderman"]
+
+let arr = 0
+while (arr < myArr.length) {
+    // console.log(`Value is ${myArr[arr]}`);
+    arr = arr + 1
+    
+}
+
+// **** Do while loop **** //
+
+let Value = 1
+do {
+    // console.log(`value is ${Value}`);
+    Value++
+    
+} while (Value <= 10);
+
+
+let values = 11 // This still works because the condition is checked after the code is executed
+do {
+    // console.log(`value is ${values}`);
+    values++
+    
+} while (values <= 10);
+
