19ef7684b7c86bb33be362e6aebf0df568059410
sandy <sandipwagle1300@gmail.com>
2024-06-07 11:39:11 +0545

    Color changer using html, css and javascript

---
+++ 1_colorchanger/script.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector("body")
+
+buttons.forEach(function(button){
+    console.log(button);
+    button.addEventListener('click', function(a){
+        console.log(a);
+        console.log(a.target);
+        if(a.target.id === 'grey'){
+            body.style.backgroundColor = a.target.id;
+        }
+        if(a.target.id === 'white'){
+            body.style.backgroundColor = a.target.id;
+        }
+        if(a.target.id === 'blue'){
+            body.style.backgroundColor = a.target.id;
+        }
+        if(a.target.id === 'yellow'){       
+            body.style.backgroundColor = a.target.id;
+        }
+
+    });
+
+});
